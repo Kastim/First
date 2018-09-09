0b22fb472dde04385cb7e39734508111fe2b58d3
Kastim <k.kardonec@mail.ru>
2018-09-09 13:45:00 +0300

    ide 1 commit
    Signed-off-by: Kastim <k.kardonec@mail.ru>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
 //import java.io.IOException;
-<<<<<<< HEAD
-// xax
-=======
 
->>>>>>> parent of 106df13... xa
 public class Main {
 
     public static void main(String[] args) throws Exception {
