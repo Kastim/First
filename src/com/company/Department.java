c1c4fcfb727c53876a2e072945d9ce367d7e3d72
Kastim <k.kardonec@mail.ru>
2018-09-16 23:32:57 +0300

    ide 1 commit
    Signed-off-by: Kastim <k.kardonec@mail.ru>

--- src/com/company/Department.java
+++ src/com/company/Department.java
@@ -9,31 +9,32 @@ import java.util.Comparator;
  * Класс не хранит явным образом номер подразделения и имя организации, частью которой является
  */
 public class Department {
-     public static void main(String[] Args) {
+    //public static void main(String[] Args) {
 
-        // Приватные поля класса (доступные только внутри класса):
-        // Разные подразделения имеют разные имена.
-        String name;
-        //Класс хранит явным образом массив своих работников.
-        ArrayList employees;
+    // Приватные поля класса (доступные только внутри класса):
+    // Разные подразделения имеют разные имена.
+    private String name;
+    //Класс хранит явным образом массив своих работников.
+    private ArrayList<Employee> employees;
 
-        // Конструктор может принимать имя и массив работников.
-        //public Department(){
-        }
-        //public Department(String name, ArrayList employees){
-          //  this.name = name;
-            //this.employees = employees;
+    // Конструктор может принимать имя и массив работников.
+    public Department() {
+    }
 
-        Department department0 = new Department();
-        department0.name = "Production";
-
+    public Department(String name, ArrayList<Employee> employees) {
+        this.name = name;
+        this.employees = new ArrayList<Employee>();
+    }
+    //Department department0 = new Department();
+    //department0 name = "Production";
+}
 
         //Публичный класс Employee - работник некоторой организации.
         //Класс не хранит явным образом номер или имя подразделения и организации, в которой работает работник.
        class Employee {
 
             // Каждый работник характеризуется фамилией.
-            private String lastName;
+            protected String lastName;
             // Каждый работник получает определенное жалование.
             private double salary;
             private int workingHours;
@@ -53,15 +54,27 @@ public class Department {
                 public double getTotalSalary() {
                     return salary * workingHours;
             }
-                Employee employee0 = new Employee("Ivanov",20,50);
-                Employee employee1 = new Employee("Petrov",10,50);
-                Employee employee2 = new Employee("Sidorov",10,70);
 
-                ArrayList <Employee> employees = new ArrayList <>();
-                ArrayList employees.add(employee0);
-                employees.add(employee1);
 
             }
+            class TestDepartmen{
+                  public static void main(String[] args) {
+
+                    Employee employee0 = new Employee("Ivanov",20,50);
+                    Employee employee1 = new Employee("Petrov",10,50);
+                    Employee employee2 = new Employee("Sidorov",10,70);
+
+
+                ArrayList <Employee> employees = new ArrayList <>();
+                    employees.add(employee0);
+                    employees.add(employee1);
+                    employees.add(employee2);
+
+
+                 Department department0 = new Department("Production",ArrayList (<Employee> employees);
+
+
+
+
         }
     }
-}
